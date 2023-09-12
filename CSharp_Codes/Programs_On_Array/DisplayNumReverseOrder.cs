519ab03e4eb91038b55a4713831183fd7858f1c9
prasadp <prasadp@DESKTOP-VDLHQ4T>
2023-09-12 13:26:44 +0530

    Commit For Programs on Array

--- CSharp_Codes/Programs_On_Array/DisplayNumReverseOrder.cs
+++ CSharp_Codes/Programs_On_Array/DisplayNumReverseOrder.cs
@@ -16,18 +16,33 @@ namespace CSharp_Codes.Programs_On_Array
 
 			int[] arr = new int[size];
 
-			Console.Write("Enter Array Elements : ");
+			Console.WriteLine("Enter Array Elements : ");
 			for (int i = 0; i < size; i++)
 			{
 				arr[i] = Convert.ToInt32(Console.ReadLine());
 			}
 
 			Console.Write("Array in normal order : ");
+			printArray(arr, size);
+
+			reverseArray(arr, size);
+			Console.WriteLine();
+
+			Console.Write("Array in rverse order : ");
+			printArray(arr, size);
+
+			Console.ReadLine();
+		}
+
+		static void printArray(int[] arr, int size)
+		{
 			for (int i = 0; i < size; i++)
 			{
-				Console.Write(arr[i] + " ");
+				Console.Write(arr[i] + "  ");
 			}
-
+		}
+		static void reverseArray(int[] arr, int size)
+		{
 			int start = 0;
 			int end = size - 1;
 			while (start < end)
@@ -38,14 +53,6 @@ namespace CSharp_Codes.Programs_On_Array
 
 				start++;
 				end--;
-			}
-
-			Console.WriteLine();
-
-			Console.Write("Array in rverse order : ");
-			for (int i = 0; i < size; i++)
-			{
-				Console.Write(arr[i] + " ");
 			}
 		}
 	}
---
+++ CSharp_Codes/Programs_On_Array/sumOfArrayElements.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Codes.Programs_On_Array
+{
+	internal class sumOfArrayElements
+	{
+		static void Main(string[] args)
+		{
+			// Take size veriable
+			int size;
+
+			//Take size of array from user
+			Console.Write("Enter the size of Array : ");
+			size = Convert.ToInt32(Console.ReadLine());
+
+			//Array Declareation
+			int[] arr = new int[size];
+
+			//Take values in array from user
+			Console.WriteLine("Enter Array Elements : ");
+			for (int i = 0; i < size; i++)
+			{
+				arr[i] = Convert.ToInt32(Console.ReadLine());
+			}
+
+			Console.Write("Array : |");
+			//Send array and size to print array function and take sum of elements from this method
+			int Sum = printArray(arr, size);
+
+			//Print sum of array elements 
+			Console.WriteLine($"\nSummation of array elements : {Sum}");
+		}
+
+		static int printArray(int[] arr, int size)
+		{
+			int Sum = 0;
+
+			//For loop to print array elements are calculate sum of array.
+			for (int i = 0; i < size; i++)
+			{
+				Console.Write(arr[i] + "|");
+				Sum += arr[i];
+			}
+
+			// Return sum to main
+			return Sum;
+		}
+	}
+}
