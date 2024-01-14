b4b7718ebb4274cee41cb76d7edbf507ed4da1a6
Prasadp20 <prasad.pathak5555@gmail.com>
2024-01-14 22:08:15 +0530

    Commit

---
+++ CSharp_Codes/C#_Logical_Programs/PalindromeString.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Codes.C__Logical_Programs
+{
+    internal class PalindromeString
+    {
+        // Check given String is palindrome or not
+
+        public static bool IsPalindrome(char[] carr, int start, int end) 
+        {
+            while (start < end)
+            {
+                if (carr[start] == carr[end])
+                {
+                    start++;
+                    end--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write( "Enter the string : ");
+            string userString = Console.ReadLine();
+
+            char[] charArray = userString.ToCharArray();
+            bool ret = IsPalindrome(charArray, 0, charArray.Length - 1);
+
+            if (ret)
+            {
+                Console.WriteLine("String is Palindrome\n");
+            }
+            else
+            {
+                Console.WriteLine("String is Not Palindrome\n");
+            }
+
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ CSharp_Codes/Programs_On_Array/valueEqualToIndex.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Codes.Programs_On_Array
+{
+    internal class valueEqualToIndex
+    {
+        public static int valueEqualToIndex1(int[] array, int num)
+        {
+            for(int i = 1; i <= num; i++)
+            {
+                if (array[i - 1] == i)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Input:\nN = ");
+            n = Convert.ToInt32(Console.ReadLine()); 
+            int[] arr = new int[n];
+
+            Console.Write("Enter Array Ele : ");
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());   
+            }
+
+            int ret = valueEqualToIndex1(arr, n);
+            Console.WriteLine($"Output : {ret}");
+        }
+    }
+}
